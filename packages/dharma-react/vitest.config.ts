037fc1cfd1765ba07186862ca712c6b9e442d153
Frans Ekman <fransedvinekman@gmail.com>
2025-08-15 00:15:40 +0200

    configure vitest aliases

--- packages/dharma-react/vitest.config.ts
+++ packages/dharma-react/vitest.config.ts
@@ -1,7 +1,12 @@
+import path from "path";
 import { defineProject } from "vitest/config";
 
 export default defineProject({
   test: {
     environment: "jsdom",
+    alias: {
+      react: path.resolve(__dirname, "node_modules/react"),
+      "dharma-core": path.resolve(__dirname, "node_modules/dharma-core"),
+    },
   },
 });
