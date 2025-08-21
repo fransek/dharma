67ae7a6095ffbe910e5b4e86e257d6cce026d09b
Frans Ekman <fransedvinekman@gmail.com>
2025-08-21 22:48:02 +0200

    removed react-demo

--- apps/react-demo/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [tanstackRouter(), react()],
-});
