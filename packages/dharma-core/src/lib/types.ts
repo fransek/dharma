86a5c5f9e2bf2403c259520689df70565e5adf77
Frans Ekman <fransedvinekman@gmail.com>
2025-09-09 15:51:27 +0200

    refactor: move types to dedicated files (#50)
    This pull request updates the documentation for the `createStore`
    function in `dharma-core`, making minor improvements to terminology and
    updating source references. It also regenerates the navigation and
    search asset files to reflect these documentation changes.
    
    Documentation improvements:
    
    * Changed the terminology in the example from "event handlers" to "event
    listeners" for clarity in `docs/functions/dharma-core.createStore.html`.
    * Updated the "Defined in" source link to point to the correct line
    number in the latest version of `createStore.ts` in
    `docs/functions/dharma-core.createStore.html`.
    
    Asset updates:
    
    * Regenerated `docs/assets/navigation.js` and `docs/assets/search.js` to
    reflect the latest documentation changes.
    [[1]](diffhunk://#diff-bb0ba3d2309006af6e40c87ae177f4a505dac45815a52c6ec6d43303bb92a39aL1-R1)
    [[2]](diffhunk://#diff-678a9a2b31f668a78c97cca14bc24776e34f86cb123f2789359e9e78f219d6d1L1-R1)

---
+++ packages/dharma-core/src/lib/types.ts
@@ -0,0 +1,98 @@
+export type Listener<TState extends object> = (state: TState) => void;
+
+export type SetState<TState extends object> = (
+  stateModifier: StateModifier<TState>,
+) => TState;
+
+export type Store<TState extends object, TActions extends object> = {
+  /** Returns the current state of the store. */
+  get: () => TState;
+  /** Sets the state of the store. */
+  set: SetState<TState>;
+  /** Resets the state of the store to its initial value. */
+  reset: () => TState;
+  /** Actions that can modify the state of the store. */
+  actions: TActions;
+  /** Subscribes to changes in the state of the store. Returns an unsubscribe function. */
+  subscribe: (listener: Listener<TState>) => () => void;
+};
+
+export type StoreEventContext<TState extends object> = {
+  /** The current state of the store. */
+  state: TState;
+  /** Sets the state of the store. */
+  set: SetState<TState>;
+  /** Resets the state of the store to its initial value. */
+  reset: () => TState;
+};
+
+export type StoreEventListener<TState extends object> = (
+  context: StoreEventContext<TState>,
+) => void;
+
+export type StateModifier<TState extends object> =
+  | Partial<TState>
+  | ((state: TState) => Partial<TState>);
+
+export type StateHandler<TState extends object> = {
+  /** Returns the current state of the store. */
+  get: () => TState;
+  /** Sets the state of the store. */
+  set: SetState<TState>;
+  /** Resets the state of the store to its initial value. */
+  reset: () => TState;
+};
+
+export type DefineActions<TState extends object, TActions> = (
+  stateHandler: StateHandler<TState>,
+) => TActions;
+
+export type BaseConfig<TState extends object, TActions extends object> = {
+  /** The initial state of the store. */
+  initialState: TState;
+  /** A function that defines actions that can modify the state. */
+  defineActions?: DefineActions<TState, TActions>;
+  /** Invoked when the store is created. */
+  onLoad?: StoreEventListener<TState>;
+  /** Invoked when the store is subscribed to. */
+  onAttach?: StoreEventListener<TState>;
+  /** Invoked when the store is unsubscribed from. */
+  onDetach?: StoreEventListener<TState>;
+  /** Invoked whenever the state changes. */
+  onChange?: StoreEventListener<TState>;
+};
+
+export type MaybePromise<T> = T | Promise<T>;
+
+export type StorageAPI = {
+  getItem: (key: string) => MaybePromise<string | null>;
+  setItem: (key: string, value: string) => MaybePromise<void>;
+  removeItem: (key: string) => MaybePromise<void>;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Serializer<T = any> = {
+  stringify: (value: T) => string;
+  parse: (value: string) => T;
+};
+
+export type NoPersistConfig = {
+  /** Whether to persist the state of the store. */
+  persist?: false;
+};
+
+export type PersistConfig<TState extends object> = {
+  /** Whether to persist the state of the store. */
+  persist: true;
+  /** The unique key used to identify this store in storage. */
+  key: string;
+  /** The storage to use for persisting the state. Defaults to local storage if available. */
+  storage?: StorageAPI;
+  /** The serializer to use for storing the state. Defaults to JSON. */
+  serializer?: Serializer<TState>;
+};
+
+export type StoreConfig<
+  TState extends object,
+  TActions extends object,
+> = BaseConfig<TState, TActions> & (PersistConfig<TState> | NoPersistConfig);
