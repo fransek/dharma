775df6e85f3261f6094fbc3aa3a0963da39b362c
Frans Ekman <fransedvinekman@gmail.com>
2025-09-15 22:02:04 +0200

    sitemap

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://dharma.fransek.dev",
   integrations: [
     starlight({
       title: "Dharma",
