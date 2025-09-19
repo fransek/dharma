7d53888c7e04e55ab919a7fa4f25eb5611337b95
Frans Ekman <fransedvinekman@gmail.com>
2025-09-19 21:05:33 +0200

    fix: improved types for context api (#90)
    ## Description
    
    Makes store context types easier to read and write.
    
    ## Related issue(s)
    
    ## Checklist before merge
    
    _Check for yes or N/A_
    
    - [x] PR title has format `type: summary`\*
    - [x] Changeset created if type is `feat` or `fix`
    - [x] New code is covered by unit tests
    - [x] Documentation updated
    
    \*Valid types: feat, fix, chore, test, refactor, docs

--- packages/dharma-react/src/lib/createStoreContext.ts
+++ packages/dharma-react/src/lib/createStoreContext.ts
@@ -6,14 +6,17 @@ import { StoreContext } from "../types/types";
  * Creates a store context with an instantiation function.
  * Useful if you need to initialize a store with dynamic data like props, or if you need to create multiple instances of the same store.
  *
- * @param {(...args: TArgs) => Store<TState, TActions>} createStore - A function that returns a new store instance.
- * @returns {StoreContext<TArgs, TState, TActions>} A store context object with the given instantiation function.
+ * @param {TCreateStore} createStore - A function that returns a new store instance.
+ * @returns {StoreContext<TCreateStore>} A store context object with the given instantiation function.
  *
  * @see {@link https://dharma.fransek.dev/react/createstorecontext/}
  */
-export const createStoreContext = <TArgs extends unknown[], TState, TActions>(
-  createStore: (...args: TArgs) => Store<TState, TActions>,
-): StoreContext<TArgs, TState, TActions> => {
-  const context = createContext<Store<TState, TActions> | null>(null);
+export const createStoreContext = <
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  TCreateStore extends (...args: any[]) => Store<any, any>,
+>(
+  createStore: TCreateStore,
+): StoreContext<TCreateStore> => {
+  const context = createContext<ReturnType<TCreateStore> | null>(null);
   return Object.assign(context, { createStore });
 };
